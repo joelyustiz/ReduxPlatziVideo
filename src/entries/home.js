685370f3e67e821eee0c60f817718c252ff2294d
Joel Yustiz <yustizjoel17@gmail.com>
2018-02-11 19:37:33 -0400

    Agregando un store a platzi video

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
 // import Playlist from './src/playlist/components/playlist'
-import Data from '../api.json'
+import data from '../api.json'
 import Home from '../pages/containers/home'
+import {createStore} from 'redux'
 
+const initialState = {
+    data:{
+        ...data
+    }
+}
+
+const store = createStore(
+    (state) => state,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 const app = document.getElementById('home-container')
 
+console.log(store.getState())
 
-render( < Home data={Data}/> , app);
+render( < Home data={data}/> , app);
